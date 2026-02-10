b6a01152207614f36867503e1170e4fad8a5e96c
Molly Wik <mollywi@student.chalmers.se>
2026-02-10 14:41:09 +0100

    Finalized tests

--- Labs/src/test/java/ScaniaTest.java
+++ Labs/src/test/java/ScaniaTest.java
@@ -5,18 +5,21 @@ class ScaniaTest {
     private Scania scania = new Scania();
 
     @org.junit.jupiter.api.Test
-    void raiseBed(){
-
-    }
-
-    @org.junit.jupiter.api.Test
-    void lowerBed(){
-
+    void bed(){
+        scania.raiseBed();
+        assertEquals(1, scania.getBedAngle());
+        scania.lowerBed();
+        assertEquals(0, scania.getBedAngle());
     }
 
     @org.junit.jupiter.api.Test
     void move(){
-
+        scania.currentSpeed = 0;
+        scania.raiseBed();
+        scania.currentSpeed = 1;
+        scania.move();
+        assertEquals(0, scania.dx);
+        assertEquals(0, scania.dy);
     }
 
 }
--- Labs/src/test/java/VehicleStorageTest.java
+++ Labs/src/test/java/VehicleStorageTest.java
@@ -6,11 +6,23 @@ class VehicleStorageTest {
 
     @org.junit.jupiter.api.Test
     void loadCar(){
+        Saab95 s = new Saab95();
+        assertTrue(storage.loadCar(s));
+        storage.loadCar(s);
+        storage.loadCar(s);
+        storage.loadCar(s);
+        storage.loadCar(s);
+        assertFalse(storage.loadCar(s));
 
     }
 
     @org.junit.jupiter.api.Test
     void unloadCar(){
+        assertNull(storage.unloadCar());
+
+        Saab95 s = new Saab95();
+        storage.loadCar(s);
+        assertEquals(storage.unloadCar(), s);
 
     }
 
