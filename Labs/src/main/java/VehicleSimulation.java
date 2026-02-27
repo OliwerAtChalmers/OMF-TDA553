3587ea66a8a4252b71925316cb70cbeee328e2ef
Molly Wik <mollywi@student.chalmers.se>
2026-02-27 13:17:08 +0100

    Refactoring
    I did basically nothing

---
+++ Labs/src/main/java/VehicleSimulation.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class VehicleSimulation {
+
+    private ArrayList<Vehicle> vehicles;
+    private tuple worldDimensions;
+}
---
+++ Labs/src/main/java/VehicleState.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public class VehicleState {
+
+    private String name;
+    private Point position;
+
+    public getPos(){return position;}
+
+    public getName(){return name;}
+}
