c84808cabc309b2d13233c5d2de7ef42a44ff690
Ollie <oliwerso@chalmers.se>
2026-03-02 11:23:25 +0100

    added spriteloader

--- Labs/src/main/java/SpriteLoader.java
+++ Labs/src/main/java/SpriteLoader.java
@@ -1,3 +1,24 @@
-public class SpriteLoader {
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.ArrayList;
 
+public class SpriteLoader {
+    public BufferedImage load(String fileName){
+        try {
+            return ImageIO.read(DrawPanel.class.getResourceAsStream("/pics/" + fileName));
+        } catch (IOException ex)
+        {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    public ArrayList<BufferedImage> loadSprites(ArrayList<String> fileNames){
+        ArrayList<BufferedImage> sprites = new ArrayList<>();
+        for (String fileName : fileNames)
+            sprites.add(load(fileName));
+
+        return sprites;
+    }
 }
